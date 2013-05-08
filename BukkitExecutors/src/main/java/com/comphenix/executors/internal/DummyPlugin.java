dd301c2315820fbeee38644d2404cd9807082ecb
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-05-08 02:28:14 +0200

    Make it possible to build this library as a Bukkit plugin.

---
+++ BukkitExecutors/src/main/java/com/comphenix/executors/internal/DummyPlugin.java
@@ -0,0 +1,12 @@
+package com.comphenix.executors.internal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A dummy plugin class for loading this as a library plugin.
+ * 
+ * @author Kristian
+ */
+public class DummyPlugin extends JavaPlugin {
+
+}
